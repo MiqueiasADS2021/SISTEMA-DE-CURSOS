f95f02428b77e9812857475983e1d17e05f7f46e
MiqueiasSoaresCardoso <miqueiasccccc@gmail.com>
2026-05-28 14:21:10 -0300

    service

--- src/main/java/br/edu/cursosifpb/controller/CursoController.java
+++ src/main/java/br/edu/cursosifpb/controller/CursoController.java
@@ -2,6 +2,7 @@ package br.edu.cursosifpb.controller;
 
 import br.edu.cursosifpb.model.Curso;
 import br.edu.cursosifpb.repository.CursoRepository;
+import br.edu.cursosifpb.service.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,20 +17,23 @@ public class CursoController {
     @Autowired
     private CursoRepository cursoRepository;
 
+    @Autowired
+    private CursoService cursoService;
+
     @GetMapping
     public ResponseEntity<List<Curso>> listarCursos() {
-        return ResponseEntity.ok(cursoRepository.findAll());
+        return ResponseEntity.ok(cursoService.listar());
     }
 
     @PostMapping("/cadastrar")
-    public ResponseEntity<String> cadastrarCurso(@RequestBody Curso c) {
-        cursoRepository.save(c);
+    public ResponseEntity<String> cadastrarCurso(@RequestBody Curso curso) {
+        cursoService.cadastrarCurso(curso);
         return ResponseEntity.status(201).body("Curso cadastrado com sucesso!");
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> buscarCurso(@PathVariable Long id) {
-        Optional<Curso> curso = cursoRepository.findById(id);
+        Optional<Curso> curso = cursoService.buscarCursoPorId(id);
 
         if (curso.isPresent()) {
             return ResponseEntity.ok(curso.get());
@@ -40,8 +44,8 @@ public class CursoController {
 
     @DeleteMapping("/remover/{id}")
     public ResponseEntity<?> deleteCurso(@PathVariable long id) {
-        if (cursoRepository.existsById(id)) {
-            cursoRepository.deleteById(id);
+        if (cursoService.buscarCursoPorId(id).isPresent()) {
+            cursoService.removerCurso(id);
             return ResponseEntity.ok("Curso removido com sucesso!");
         } else {
             return ResponseEntity.status(404).body("Curso não encontrado para o id: " + id);
@@ -50,9 +54,9 @@ public class CursoController {
 
     @PutMapping("/editar/{id}")
     public ResponseEntity<?> editarCurso(@PathVariable long id, @RequestBody Curso cursoAtualizado) {
-        if (cursoRepository.existsById(id)) {
+        if (cursoService.buscarCursoPorId(id).isPresent()) {
             cursoAtualizado.setId(id);
-            cursoRepository.save(cursoAtualizado);
+            cursoService.atualizarCurso(id,cursoAtualizado);
             return ResponseEntity.ok("Curso atualizado com sucesso!");
         } else {
             return ResponseEntity.status(404).body(
@@ -63,27 +67,11 @@ public class CursoController {
 
     @PatchMapping("/editar/{id}")
     public ResponseEntity<?> editarCursoParcial(@PathVariable long id, @RequestBody Curso cursoAtualizado) {
-
-        if (cursoRepository.existsById(id)) {
-
-            Curso curso = cursoRepository.findById(id).get();
-
-            if (cursoAtualizado.getNome() != null && !cursoAtualizado.getNome().isBlank()) {
-                curso.setNome(cursoAtualizado.getNome());
-            }
-
-            if (cursoAtualizado.getCargaHoraria() != null) {
-                curso.setCargaHoraria(cursoAtualizado.getCargaHoraria());
-            }
-
-            cursoRepository.save(curso);
-
+        if(cursoService.buscarCursoPorId(id).isPresent()) {
+            cursoService.atualizarCursoParcial(id,cursoAtualizado);
             return ResponseEntity.ok("Curso atualizado com sucesso!");
-
-        } else {
-            return ResponseEntity.status(404).body(
-                    "Falha na atualização de curso! Verifique se o curso existe para o id: " + id
-            );
+        }else{
+            return ResponseEntity.status(404).body("Curso não encontrado para o id: " + id);
         }
     }
 }
---
+++ src/main/java/br/edu/cursosifpb/service/CursoService.java
@@ -0,0 +1,63 @@
+package br.edu.cursosifpb.service;
+
+import br.edu.cursosifpb.model.Curso;
+import br.edu.cursosifpb.repository.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CursoService {
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    public List<Curso> listar(){
+        return cursoRepository.findAll();
+    }
+
+    public Curso cadastrarCurso(Curso curso){
+        return cursoRepository.save(curso);
+    }
+
+    public Optional<Curso> buscarCursoPorId(Long id){
+        return cursoRepository.findById(id);
+    }
+
+    public boolean removerCurso(Long id){
+        if(cursoRepository.existsById(id)){
+            cursoRepository.deleteById(id);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public Optional<Curso> atualizarCurso(Long id, Curso curso){
+        if(cursoRepository.existsById(id)){
+            curso.setId(id);
+            Curso curso_salvo = cursoRepository.save(curso);
+            return Optional.of(curso_salvo);
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Curso> atualizarCursoParcial(Long id, Curso cursoAtualizado){
+        if(cursoRepository.existsById(id)){
+            Optional<Curso> curso_atual = cursoRepository.findById(id);
+            if(cursoAtualizado.getNome() != null){
+                curso_atual.get().setNome(cursoAtualizado.getNome());
+            }
+            if(cursoAtualizado.getCargaHoraria() != null){
+                curso_atual.get().setCargaHoraria(cursoAtualizado.getCargaHoraria());
+            }
+
+            Curso curso_salvo = cursoRepository.save(curso_atual.get());
+            return Optional.of(curso_salvo);
+        }
+        return Optional.empty();
+    }
+
+}
